7db04a6461bc0907ec8691c563e63b1f1c4f0973
jinzhixiao <jinzhixiao@weli.cn>
2019-03-26 21:24:18 +0800

    ArrayList和LinkedList的源码阅读笔记

--- javathinking4/src/Main.java
+++ javathinking4/src/Main.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
         Integer[][] a4 = {       { new Integer(1), new Integer(2)},       { new Integer(3), new Integer(4)},       { new Integer(5), new Integer(6)},     };
         System.out.println(a4.length);
         System.out.println(a4[0].length);
+        List list=new ArrayList<>();
+        list=new LinkedList();
     }
 }
